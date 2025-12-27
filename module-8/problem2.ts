d6cea12ec1be059c11df9fc0bc8b70644da5b1da
always-mohammad-ali <always.mohammad.ali@gmail.com>
2025-12-27 09:37:58 +0600

    string or array length related practice

--- module-8/problem2.ts
+++ module-8/problem2.ts
@@ -9,3 +9,20 @@ function getLength(value : string | (string | number)[]) : number{
 console.log(getLength("hey hi ho  "));
 console.log(getLength([4, 6, 98, 532, 1, 0, -5]));
 console.log(getLength(["ali", "hi",".", "said", "mirza kader"]));
+
+
+function getValueLength(value : string | (number | string)[]) : string {
+
+    if(typeof value == "string"){
+        return `it is string bro and its length is ${value.length} and ${value}`; 
+        
+    }
+     
+    return `it may string array or number array and its length is ${value.length} and ${value}`
+   
+    
+}
+
+console.log(getValueLength("hello good morning, you have a nice looking eye"));
+console.log(getValueLength([45, 32, 5444]));
+console.log(getValueLength(["an", "up"]));
